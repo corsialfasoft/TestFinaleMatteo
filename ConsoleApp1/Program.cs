667d6d308fb953db2445296e4d2a3622de9e1996
mtani93 <matteo.cecina@gmail.com>
2018-05-09 14:06:00 +0200

    esercizio 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,17 @@ using System.Threading.Tasks;
 namespace ConsoleApp1 {
 	class Program {
 		static void Main(string[] args) {
+			int i=2;
+			while(i<31){
+				Console.WriteLine("{0}",i);
+				i= i+2;
+			}
+			Console.WriteLine("-----------");
+			i= i-3;
+			while(i>0){
+				Console.WriteLine("{0}",i);
+				i = i-2;
+			}
 		}
 	}
 }
