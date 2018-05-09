e77b07dd91a34cee2f1a4aabe0a55f4e2ba6684b
mtani93 <matteo.cecina@gmail.com>
2018-05-09 17:38:24 +0200

    inizio stored

---
+++ esercizio mvc/ddl.sql
@@ -0,0 +1,17 @@
+﻿create database Ristorante;
+use Ristorante;
+
+create table Menu(
+Id int identity(1,1) primary key not null,
+Primo nvarchar(50),
+Secondo nvarchar(50),
+Contorno nvarchar(50),
+Dolce nvarchar(50)
+);
+
+create table Giornata(
+Id int identity(1,1) primary key not null,
+Giorno nvarchar(50),
+Orario nvarchar(50),
+FkMenu int foreign key references Menu 
+);
---
+++ esercizio mvc/sp.sql
@@ -0,0 +1,23 @@
+﻿create procedure AggiungiGiorno
+@Giorno nvarchar(50),
+@Orario nvarchar(50)
+as
+	insert into Giornata values(@Giorno,@Orario,null);
+go
+exec AggiungiGiorno 'mock', 'mock';
+
+create procedure AggiungiMenu
+@Primo nvarchar(50),
+@Secondo nvarchar(50),
+@Contorno nvarchar(50),
+@Dolce nvarchar(50),
+@Giorno nvarchar(50),
+@Orario nvarchar(50)
+as
+	insert into Menu values (@Primo,@Secondo,@Contorno,@Dolce);
+	declare @corrente int;
+	set @corrente = ident_current('Menu');
+	update Giornata set FkMenu=@corrente where Giorno=@Giorno and Orario=@Orario;
+go
+
+exec AggiungiMenu 'Mock','Mock','Mock','Mock','Mock','Mock'
